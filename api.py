7d4e15dc45c0dc2fde336980d8ab1e8317683a51
Samia Aldejwi <123486948+samdej01@users.noreply.github.com>
2025-12-12 12:02:43 +0300

    Update app run configuration to use environment port
    Fix Flask app for local and Render deployment

--- api.py
+++ api.py
@@ -32,5 +32,5 @@ def predict_sms():
     }), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
-
+    port = int(os.environ.get("PORT", 50001))  
+    app.run(host="0.0.0.0", port=port, debug=True)
